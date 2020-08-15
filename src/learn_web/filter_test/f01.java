042f6e584a0ed70a7f9d040ad962deb819404ead
jonekaka <973579614@qq.com>
2020-08-15 09:30:14 +0800

    学习一些web知识

---
+++ src/learn_web/filter_test/f01.java
@@ -0,0 +1,25 @@
+package learn_web.filter_test;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+@WebFilter("/*")
+public class f01 implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("filter执行");
+//        放行
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
